a834a3a62c9b7078376744800440c445c20ba598
SergioMisas <misas_virseda@hotmail.es>
2025-01-14 20:01:21 +0100

    Scaffold

---
+++ Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.sergiomisas.shoppingventory
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.sergiomisas.shoppingventory.ui.theme.ShoppingventoryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingventoryTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable("home") {
+                        Scaffold {
+                            Greeting("Android", Modifier.padding(it))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Serializable
+object HomeScreen
