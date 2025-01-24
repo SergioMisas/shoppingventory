b1259011fca00f1eead21e71358719759581b73a
SergioMisas <misas_virseda@hotmail.es>
2025-01-24 17:28:50 +0100

    Added Supermarket.kt and included it in Item.kt

--- Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Item.kt
+++ Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Item.kt
@@ -3,7 +3,7 @@ package dev.sergiomisas.shoppingventory.models
 data class Item(
     val name: String,
     val price: Double,
-    val supermarket: String,
+    val supermarket: Supermarket,
     val quantity: Int = 0,
     val picture: String
 )
---
+++ Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Supermarket.kt
@@ -0,0 +1,6 @@
+package dev.sergiomisas.shoppingventory.models
+
+data class Supermarket(
+    val id: String,
+    val name: String
+)
