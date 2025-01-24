64d0dfde83ed71edfa415064c219c01d52195798
SergioMisas <misas_virseda@hotmail.es>
2025-01-24 17:21:30 +0100

    Added creator to Household.kt

--- Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Household.kt
+++ Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Household.kt
@@ -3,6 +3,7 @@ package dev.sergiomisas.shoppingventory.models
 data class Household (
     val id: String,
     val name: String,
+    val creator: User,
     val admins: MutableList<User> = mutableListOf<User>(),
     val members: MutableList<User> = mutableListOf<User>(),
     val items: MutableList<Item> = mutableListOf<Item>(),
