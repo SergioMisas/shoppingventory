f37932dca5ea2770dd3f079f244a0d1bdd9b4d93
SergioMisas <misas_virseda@hotmail.es>
2025-01-24 18:04:50 +0100

    Updated Item.kt with Item.quantityToShop and Item.quantityAvailable

--- Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Item.kt
+++ Shoppingventory/app/src/main/java/dev/sergiomisas/shoppingventory/models/Item.kt
@@ -4,6 +4,7 @@ data class Item(
     val name: String,
     val price: Double,
     val supermarket: Supermarket,
-    val quantity: Int = 0,
+    val quantityAvailable: Int = 0,
+    val quantityToShop: Int = 0,
     val picture: String
 )
